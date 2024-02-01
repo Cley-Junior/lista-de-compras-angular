dc9758bc41cd5eac4ba326619978addbcba21d4e
Cley Junior <jr.bcley@gmail.com>
2024-02-01 18:27:13 -0300

    Lista de Compras com Funcionalidades Iniciais Completas

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy
+{
+  @Input()
+  item!: Item;
+
+  @Output()
+  emitindoItemParaEditar = new EventEmitter();
+
+  @Output()
+  emitindoItemParaDeletar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash
@@ -15,4 +25,30 @@ export class ItemComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  ngOnChanges(): void {}
+
+  editarItem()
+  {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+
+  checarItem()
+  {
+    if (this.item.comprado == true)
+    {
+      this.item.comprado = false;
+    } else {
+      this.item.comprado = true;
+    }
+  }
+
+  deletarItem()
+  {
+    this.emitindoItemParaDeletar.emit(this.item.id);
+  }
+
+  ngOnDestroy(): void
+  {
+
+  }
 }
